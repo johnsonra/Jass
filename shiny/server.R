0e835f9dbc3ab3952c01703b90242e964ed9e31c
Randy Johnson <johnsonra@mail.nih.gov>
2021-09-30 19:56:16 -0400

    Update from hand to card annotation

--- shiny/server.R
+++ shiny/server.R
@@ -17,55 +17,57 @@ library(magick)
 root <- system('git rev-parse --show-toplevel', intern = TRUE)
 cards <- paste0(rep(c('B', 'R', 'S', 'E'), each = 9), rep(c(6:9, 'B', 'U', 'O', 'K', 'A'), 4))
 d <- format(Sys.time(), '%Y-%m-%d')
+images <- '/cards/'
+placeholder_image <- '/shiny/tmp.jpg'
+annotations <- 'cards.csv'
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
     observeEvent(input$pic, {
         # clear file if it exists
-        if(file.exists(paste0(root, '/raw_hands/', input$file)))
-            file.remove(paste0(root, '/raw_hands/', input$file))
+        if(file.exists(paste0(root, images, input$file)))
+            file.remove(paste0(root, images, input$file))
 
         # take picture
         paste('ffmpeg -ss 0.5 -f avfoundation -framerate 30 -i "0" -t 1 -frames 1',
-              paste0(root, '/raw_hands/', input$file)) %>%
+              paste0(root, images, input$file)) %>%
             system()
 
         output$img <- renderPlot(
             {
                 par(mar = rep(0, 4))
 
-                if(file.exists(paste0(root, '/raw_hands/', input$file)))
+                if(file.exists(paste0(root, images, input$file)))
                 {
-                    plot(image_read(paste0(root, '/raw_hands/', input$file)))
+                    plot(image_read(paste0(root, images, input$file)))
                 }else{
-                    plot(image_read(paste0(root, '/shiny/tmp.jpg')))
+                    plot(image_read(paste0(root, placeholder_image)))
                 }
             })
     })
 
-    observeEvent(input$game, {
+    observeEvent(input$set, {
         # file name
-        updateTextInput(session, 'file', value = paste0(d, '_', input$game, '_', input$hand, '.jpg'))
+        updateTextInput(session, 'file', value = paste0(d, '_', input$set, '_', input$card, '.jpg'))
     })
 
-    observeEvent(input$hand, {
+    observeEvent(input$card, {
         # file name
-        updateTextInput(session, 'file', value = paste0(d, '_', input$game, '_', input$hand, '.jpg'))
+        updateTextInput(session, 'file', value = paste0(d, '_', input$set, '_', input$card, '.jpg'))
     })
 
-    observeEvent(input$sav, {
-        cards_in_hand <- map_lgl(cards, ~ input[[.x]])
-
-        if(sum(cards_in_hand) == 9)
-        {
-            cat(d, ',', input$game, ',', input$hand, ',',
-                paste(as.integer(cards_in_hand), collapse = ','), '\n',
-                file = paste0(root, '/raw_hands/hands.csv'), sep = '', append = TRUE)
-        }else{
-            print(cards_in_hand)
-            warning('expecting 9 cards')
-        }
-    })
+    # observeEvent(input$sav, {
+    #     #cards_in_hand <- map_lgl(cards, ~ input[[.x]])
+    # 
+    #     # if(sum(cards_in_hand) == 9)
+    #     # {
+    #         cat(d, ',', input$set, ',', input$card, '\n',
+    #             file = paste0(root, images, annotations), sep = '', append = TRUE)
+    #     # }else{
+    #     #     print(cards_in_hand)
+    #     #     warning('expecting 9 cards')
+    #     # }
+    # })
 
     observeEvent(input$clear, {
         for(i in cards)
@@ -77,11 +79,11 @@ shinyServer(function(input, output, session) {
     {
         par(mar = rep(0, 4))
 
-        if(file.exists(paste0(root, '/raw_hands/', input$file)))
+        if(file.exists(paste0(root, images, input$file)))
         {
-            plot(image_read(paste0(root, '/raw_hands/', input$file)))
+            plot(image_read(paste0(root, images, input$file)))
         }else{
-            plot(image_read(paste0(root, '/shiny/tmp.jpg')))
+            plot(image_read(paste0(root, placeholder_image)))
         }
     })
 })
--- shiny/ui.R
+++ shiny/ui.R
@@ -19,7 +19,7 @@ dashboardPage(
     ################# Sidebar ################
     dashboardSidebar(
         sidebarMenu(
-            menuItem("Data Collection", tabName = 'annotate')
+            menuItem("Card Collection", tabName = 'annotate')
         )
     ),
 
@@ -27,67 +27,15 @@ dashboardPage(
     dashboardBody(tabItems(
         tabItem(tabName = 'annotate',
 
-                # Image hand
+                # Image cards
                 fluidRow(
                     column(3,
-                           textInput('game', 'Game number:'),
-                           textInput('hand', 'Hand number:'),
+                           textInput('set', 'Set number:'),
+                           textInput('card', 'Card:'),
                            textInput('file', '', value = 'tmp.jpg'),
-                           actionButton('clear', 'Clear Hand'),
-                           actionButton('pic', 'Capture Hand'),
-                           actionButton('sav', 'Save Hand')),
+                           actionButton('pic', 'Capture Hand')),
                     column(6,
                            plotOutput('img', height = '250px', width = '445px'))
-                ),
-
-                # Cards
-                fluidRow(h2("Cards in this hand")),
-                fluidRow(
-                    column(1),
-                    column(2,
-                           h3("Schellen"),
-                           checkboxInput('B6', '6'),
-                           checkboxInput('B7', '7'),
-                           checkboxInput('B8', '8'),
-                           checkboxInput('B9', '9'),
-                           checkboxInput('BB', 'B'),
-                           checkboxInput('BU', 'U'),
-                           checkboxInput('BO', 'O'),
-                           checkboxInput('BK', 'K'),
-                           checkboxInput('BA', 'A')),
-                    column(2,
-                           h3("Rosen"),
-                           checkboxInput('R6', '6'),
-                           checkboxInput('R7', '7'),
-                           checkboxInput('R8', '8'),
-                           checkboxInput('R9', '9'),
-                           checkboxInput('RB', 'B'),
-                           checkboxInput('RU', 'U'),
-                           checkboxInput('RO', 'O'),
-                           checkboxInput('RK', 'K'),
-                           checkboxInput('RA', 'A')),
-                    column(2,
-                           h3("Schilten"),
-                           checkboxInput('S6', '6'),
-                           checkboxInput('S7', '7'),
-                           checkboxInput('S8', '8'),
-                           checkboxInput('S9', '9'),
-                           checkboxInput('SB', 'B'),
-                           checkboxInput('SU', 'U'),
-                           checkboxInput('SO', 'O'),
-                           checkboxInput('SK', 'K'),
-                           checkboxInput('SA', 'A')),
-                    column(2,
-                           h3("Eicheln"),
-                           checkboxInput('E6', '6'),
-                           checkboxInput('E7', '7'),
-                           checkboxInput('E8', '8'),
-                           checkboxInput('E9', '9'),
-                           checkboxInput('EB', 'B'),
-                           checkboxInput('EU', 'U'),
-                           checkboxInput('EO', 'O'),
-                           checkboxInput('EK', 'K'),
-                           checkboxInput('EA', 'A'))
                 )
         )
     ))
