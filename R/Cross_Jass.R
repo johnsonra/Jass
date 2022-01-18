e47f322d5681faadb1e2e2b1830d40056a68b609
Randy Johnson <johnsonra@mail.nih.gov>
2022-01-17 19:57:44 -0500

    Fully automated round

--- R/Cross_Jass.R
+++ R/Cross_Jass.R
@@ -41,3 +41,40 @@ pick_random_valid_card <- function(trk, h, ...)
       return()
   }
 }
+
+#' Pick a random suit for trump
+#'
+#' @description This is just about the simplest function to pick trump suit.
+#' @name pick_random_trump
+#' @rdname Cross-Jass
+#'
+#' @param g A game object
+#' @param p The player number who is picking trump
+#' @param strategy A string identifying the strategy to use (see details).
+#' @param ... All arguments are ignored by this function
+#' @details Current strategies for picking a suit are:
+#' Random - Pick a suit completely at random
+#' Max points - Pick the suit worth the most points if it were trump
+#' Max cards - Pick the suit that has the most cards in the hand
+#' @return A character value of the trump suit
+#' @importFrom purrr map
+#' @export
+pick_random_trump <- function(g, p, strategy = 'Max points', ...)
+{
+  # otherwise 'Random'
+  scores <- runif(4)
+
+  if(strategy == 'Max points')
+  {
+    scores <- sapply(c('Bells', 'Flowers', 'Shields', 'Acorns'),
+                      function(.x) with(filter(cards(g, p), suit == .x), sum(card_value(face, TRUE))))
+  }
+
+  if(strategy == 'Max cards')
+  {
+    scores <- sapply(c('Bells', 'Flowers', 'Shields', 'Acorns'),
+                     function(.x) with(filter(cards(g, p), suit == .x), length(face)))
+  }
+
+  return(names(scores)[which.max(scores)])
+}
